e79d7cbaaf271da0822730695776f0b5852549a0
KOSTA-036 <KOSTA-036@KOSTA-036-PC.mshome.net>
2016-12-01 20:20:57 +0900

    master change

--- JavaPROJECT/src/HeHeHe.java
+++ JavaPROJECT/src/HeHeHe.java
@@ -8,6 +8,13 @@ public class HeHeHe {
 		int i=0;
 		int y=0;
 		String url = "www.dkcns.com";
+		
+		String string_a = new HeHeHe().returnString();
+		System.out.println(string_a);
 	}
  
+	public String returnString(){
+		return "KK";
+	}
+	
 }
